52ae4a43e8f37a3fc22772c97b4c6365da00955c
Luigi G Fonseca <luigi.gottardello.fonseca@gmail.com>
2022-10-16 22:14:07 -0300

    chore: default colors theme

---
+++ lib/src/theme/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class AppColors {
+  final white = const Color(0xEDF2FF);
+  final darkGray = const Color(0x1E1E26);
+  final lightBlue = const Color(0x395AFC);
+  final darkBlue = const Color(0x364FC7);
+
+  const AppColors();
+}
