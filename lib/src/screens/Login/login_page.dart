11d04a45e970732a46ff8406a180db2fe523271f
Luigi G Fonseca <luigi.gottardello.fonseca@gmail.com>
2022-10-22 17:03:09 -0300

    feat: submitted state function

--- lib/src/screens/Login/login_page.dart
+++ lib/src/screens/Login/login_page.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:unimeet/src/widgets/button_widget.dart';
+import 'package:unimeet/src/widgets/input_widget.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({super.key});
 
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
   final String login = 'Logar';
+
   final String register = 'Cadastrar';
+
+  final TextEditingController _emailController = TextEditingController();
+
+  final TextEditingController _passwordController = TextEditingController();
+
+  bool submitted = false;
 
   @override
   Widget build(BuildContext context) {
@@ -13,12 +26,49 @@ class Login extends StatelessWidget {
       backgroundColor: const Color(0xFF1E1E26),
       body: Container(
         margin: const EdgeInsets.only(left: 24, right: 24),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: ListView(
           children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 96),
+              child: Center(
+                child: Text(
+                  "UNIMEET",
+                  style: TextStyle(
+                    color: Color(0xFFFFFFFF),
+                    fontSize: 64,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 108,
+            ),
+            Input(
+              controller: _emailController,
+              submitted: submitted,
+              labelText: "Email",
+              typeInput: "email",
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Input(
+              controller: _passwordController,
+              submitted: submitted,
+              labelText: "Senha",
+              typeInput: "password",
+            ),
+            const SizedBox(
+              height: 40,
+            ),
             Button(
               buttonText: login,
-              handleClickButton: () => {},
+              handleClickButton: () => {
+                setState(() {
+                  submitted = true;
+                })
+              },
             ),
             const SizedBox(
               height: 16,
