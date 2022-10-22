53efc46ca86aad4fab0754298d5375d41ae31259
Luigi G Fonseca <luigi.gottardello.fonseca@gmail.com>
2022-10-22 14:58:23 -0300

    feat: login and register button

--- lib/src/screens/Login/login_page.dart
+++ lib/src/screens/Login/login_page.dart
@@ -4,20 +4,31 @@ import 'package:unimeet/src/widgets/button_widget.dart';
 class Login extends StatelessWidget {
   const Login({super.key});
 
-  final String login = 'Login';
+  final String login = 'Logar';
+  final String register = 'Cadastrar';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF1E1E26),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Button(
-            buttonText: login,
-            handleClickButton: () => {},
-          )
-        ],
+      body: Container(
+        margin: const EdgeInsets.only(left: 24, right: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Button(
+              buttonText: login,
+              handleClickButton: () => {},
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Button(
+              buttonText: register,
+              handleClickButton: () => {},
+            )
+          ],
+        ),
       ),
     );
   }
