438d8f025a102665d23369a21fd88ce24f4870b2
L <luigi.gottardello.fonseca@gmail.com>
2022-10-22 23:36:29 -0300

    style: app bar height

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -12,7 +12,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Roboto'),
-      initialRoute: '/sign-in',
+      initialRoute: '/profile',
       routes: {
         '/': (context) => const HomePage(),
         '/sign-in': ((context) => const Login()),
--- lib/src/widgets/custom_appbar.dart
+++ lib/src/widgets/custom_appbar.dart
@@ -4,7 +4,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CustomAppBar({super.key, required this.title});
   final String title;
   @override
-  Size get preferredSize => const Size.fromHeight(78);
+  Size get preferredSize => const Size.fromHeight(60);
 
   @override
   Widget build(BuildContext context) {
@@ -12,15 +12,12 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
         automaticallyImplyLeading: false,
         backgroundColor: const Color(0xFF9F14FC),
         centerTitle: true,
-        title: Padding(
-          padding: const EdgeInsets.only(top: 18),
-          child: Text(title,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                color: Color(0xFFFFFFFF),
-                fontSize: 26,
-                fontWeight: FontWeight.bold,
-              )),
-        ));
+        title: Text(title,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Color(0xFFFFFFFF),
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            )));
   }
 }
