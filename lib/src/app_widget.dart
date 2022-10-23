01733e376e18402fd81850f4cfeee3c5dac4e221
L <luigi.gottardello.fonseca@gmail.com>
2022-10-22 23:19:27 -0300

    feat: email recovery password page

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -3,6 +3,7 @@ import 'package:unimeet/src/home_page.dart';
 import 'package:unimeet/src/screens/RecoveryPassword/auth_page.dart';
 import 'package:unimeet/src/screens/Login/login_page.dart';
 import 'package:unimeet/src/screens/Profile/profile_page.dart';
+import 'package:unimeet/src/screens/RecoveryPassword/email_page.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
@@ -11,12 +12,13 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Roboto'),
-      initialRoute: '/auth',
+      initialRoute: '/password-recovery',
       routes: {
         '/': (context) => const HomePage(),
         '/sign-in': ((context) => const Login()),
         '/profile': (context) => const Profile(),
-        '/auth': (context) => const VerfiyMfa()
+        '/auth': (context) => const VerfiyMfa(),
+        '/password-recovery': (context) => const PasswordRecovery(),
       },
     );
   }
