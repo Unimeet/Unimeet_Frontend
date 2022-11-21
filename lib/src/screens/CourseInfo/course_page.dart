ef638103cd2c1634399ff10bfb177e1962a01d49
Isabela <isabela.cpedro@hotmail.com>
2022-11-21 20:34:57 -0300

    perf: add widgets to the course page

--- lib/src/screens/CourseInfo/course_page.dart
+++ lib/src/screens/CourseInfo/course_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:unimeet/src/widgets/main_text.dart';
+import 'package:unimeet/src/screens/CourseInfo/Widgets/description.dart';
 import '../../widgets/custom_appbar.dart';
 
 class CoursePage extends StatelessWidget {
@@ -9,21 +9,34 @@ class CoursePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF1E1E26),
-      appBar: const CustomAppBar(title: "Informações do curso"),
+      appBar: const CustomAppBar(title: "Ciência da computação"),
       body: Padding(
         padding: const EdgeInsets.only(left: 24, right: 24),
         child: ListView(
           children: const [
             SizedBox(height: 16),
-            MainText(title: 'Tempo médio de conclusão'),
-            SizedBox(height: 16),
-            MainText(title: 'Estágio obrigatório'),
-            SizedBox(height: 16),
-            MainText(title: 'TCC obrigatório'),
-            SizedBox(height: 16),
-            MainText(title: 'Matérias obrigatórias'),
-            SizedBox(height: 16),
-            MainText(title: 'Descrição do curso'),
+            DescriptionBox(
+              title: "Tempo médio de conclusão",
+              description: "3 anos",
+            ),
+            DescriptionBox(
+              title: "Estágio obrigatório",
+              description: "Não",
+            ),
+            DescriptionBox(
+              title: "Trabalho de Conclusão de Curso",
+              description: "Não obrigatório",
+            ),
+            DescriptionBox(
+              title: "Matérias obrigatórias",
+              description:
+                  "(teste) As do BCT, mais eletivas de vários eixos que podem ser escolhidos. Ccomp e ecomp compartilham a maioria, mas é preciso ficar atento na ementa",
+            ),
+            DescriptionBox(
+              title: "Descrição do curso",
+              description:
+                  "Muito chatinho, teorico, maçante, triste, boa sorte pois eu estou quase me retirando",
+            ),
           ],
         ),
       ),
