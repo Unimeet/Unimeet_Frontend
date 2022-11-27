c5425889a5e31afcb6b4e9cba06afe2ce5a19c6a
L <luigi.gottardello.fonseca@gmail.com>
2022-11-27 18:00:04 -0300

    fix: add response data missing, 'cellphone', 'college', 'course', 'startCourseDate', 'aboutMe', 'buddy'

--- lib/src/services/user/register_service.dart
+++ lib/src/services/user/register_service.dart
@@ -1,20 +1,27 @@
 import 'package:dio/dio.dart';
+import 'package:unimeet/src/models/user_register_college.dart';
 import 'package:unimeet/src/models/user_register_model.dart';
 import 'package:unimeet/src/utils/date_formatter.dart';
 
 const String baseURL =
     "https://0ul7agory0.execute-api.us-east-1.amazonaws.com/Prod";
 
-Future postRegisterUser(UserRegisterModel userData) async {
+Future postRegisterUser(
+    UserRegisterModel userData, UserRegisterCollege userCollegeData) async {
   try {
     final response = await Dio().post('$baseURL/api/v1/user/create', data: {
       'email': userData.email,
       'password': userData.password,
       'name': userData.name,
       'cpf': userData.cpf,
-      'birthdate': dateFormatter(userData.birthday)
+      'birthdate': dateFormatter(userData.birthday),
+      'cellphone': userData.cellphone,
+      'college': userCollegeData.college,
+      'course': userCollegeData.course,
+      'startCourseDate': userCollegeData.startCourseDate,
+      'aboutMe': userCollegeData.aboutMe,
+      'buddy': userCollegeData.buddy
     });
-    //add other fields from request here
 
     return response.statusCode;
   } on DioError catch (e) {
