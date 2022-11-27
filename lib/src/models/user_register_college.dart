21a253075187b729ed748c445c52c9fd4f1d5600
L <luigi.gottardello.fonseca@gmail.com>
2022-11-27 17:58:50 -0300

    feat: add user_register_college model

---
+++ lib/src/models/user_register_college.dart
@@ -0,0 +1,10 @@
+class UserRegisterCollege {
+  String college;
+  String course;
+  String startCourseDate;
+  String aboutMe;
+  bool? buddy;
+
+  UserRegisterCollege(this.college, this.course, this.startCourseDate,
+      this.aboutMe, this.buddy);
+}
--- lib/src/models/user_register_model.dart
+++ lib/src/models/user_register_model.dart
@@ -4,7 +4,8 @@ class UserRegisterModel {
   String cpf;
   String birthday;
   String password;
+  String cellphone;
 
-  UserRegisterModel(
-      this.name, this.email, this.cpf, this.birthday, this.password);
+  UserRegisterModel(this.name, this.email, this.cellphone, this.cpf,
+      this.birthday, this.password);
 }
