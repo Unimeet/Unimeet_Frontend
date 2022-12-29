4ade5deefe79c75aa1dd4a77c33ed0a2b022a572
L <luigi.gottardello.fonseca@gmail.com>
2022-12-29 12:09:52 -0300

    feat: profile pics

--- lib/src/screens/Feed/feed_page.dart
+++ lib/src/screens/Feed/feed_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:unimeet/src/widgets/custom_appbar.dart';
+import 'package:unimeet/src/widgets/profile_pic.dart';
 import 'package:unimeet/src/widgets/university_name_widget.dart';
 
 class Feed extends StatelessWidget {
@@ -13,9 +14,63 @@ class Feed extends StatelessWidget {
     return Scaffold(
       appBar: const CustomAppBar(title: appBarTitle),
       backgroundColor: const Color(0xFF1E1E26),
-      body: Container(
-        child: UniversityName(title: universityName),
-      ),
+      body: Card(
+          child: Padding(
+        padding:
+            const EdgeInsets.only(left: 24, right: 24, top: 16, bottom: 16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              children: [
+                ProfilePic(
+                  imageURL:
+                      "https://www.altoastral.com.br/media/uploads/legacy/2016/07/AAT001-P001-89335-1-m-Divulgacao_1.jpg",
+                  size: 40,
+                  allowBorder: false,
+                ),
+                SizedBox(width: 8),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Luigi G Fonseca",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Engenharia de Materiais",
+                      style: TextStyle(fontSize: 14, color: Color(0xFF5F5F5F)),
+                    )
+                  ],
+                ),
+                Spacer(),
+                Column(
+                  children: [
+                    Text(
+                      "01/10/2022",
+                      style: TextStyle(fontSize: 14, color: Color(0xFF5F5F5F)),
+                    ),
+                    Text(
+                      "14:24",
+                      style: TextStyle(fontSize: 14, color: Color(0xFF5F5F5F)),
+                    )
+                  ],
+                )
+              ],
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Text(
+              "Quanto tempo leva para concluir o curso de engenharia da computação? Estou querendo migrar pois não me senti a vontade em E. Materiais.",
+            ),
+            SizedBox(height: 12),
+            Row(
+              children: [Text("teste"), Text("test")],
+            )
+          ],
+        ),
+      )),
     );
   }
 }
--- lib/src/widgets/profile_pic.dart
+++ lib/src/widgets/profile_pic.dart
@@ -4,25 +4,31 @@ class ProfilePic extends StatelessWidget {
   const ProfilePic(
       {super.key,
       this.imageURL =
-          "https://s3.amazonaws.com/unimeet-dev.com.br/users/d6cf3b4f-7815-4680-b617-e93a519eca51/images/profile/image.jpg"});
+          "https://s3.amazonaws.com/unimeet-dev.com.br/users/d6cf3b4f-7815-4680-b617-e93a519eca51/images/profile/image.jpg",
+      this.size = 166,
+      this.allowBorder = true});
 
   final String imageURL;
+  final double size;
+  final allowBorder;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 166,
-      height: 166,
+      width: size,
+      height: size,
       padding: const EdgeInsets.all(1),
-      decoration: BoxDecoration(
-          color: const Color(0xFFFFFFFF),
-          borderRadius: BorderRadius.circular(30)),
+      decoration: allowBorder == true
+          ? BoxDecoration(
+              color: const Color(0xFFFFFFFF),
+              borderRadius: BorderRadius.circular(30))
+          : null,
       child: ClipRRect(
         borderRadius: BorderRadius.circular(30),
         child: SizedBox.fromSize(
           size: const Size.fromRadius(30),
           child: Image.network(
-            "https://s3.amazonaws.com/unimeet-dev.com.br/users/d6cf3b4f-7815-4680-b617-e93a519eca51/images/profile/image.jpg",
+            imageURL,
             fit: BoxFit.cover,
           ),
         ),
